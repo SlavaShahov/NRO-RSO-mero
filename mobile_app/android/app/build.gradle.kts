b0183f7a87741cfb58fa538597badca6ed544d34
SlavaShahov <Slava.Shahov04@yandex.ru>
2026-05-08 00:03:14 +0700

    fix: minSdk 23

--- mobile_app/android/app/build.gradle.kts
+++ mobile_app/android/app/build.gradle.kts
@@ -32,6 +32,7 @@ android {
     defaultConfig {
         applicationId = "ru.rso.nsk.events"
         minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
